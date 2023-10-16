3b4f94569c9c35f8ba9cc9f330f11c02f39f2fb0
Owen Hildreth <ohildreth@mines.edu>
2023-10-16 12:41:00 -0600

    Initial Commit

---
+++ PicklesCamera/PicklesCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicklesCameraApp.swift
+//  PicklesCamera
+//
+//  Created by Owen Hildreth on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct PicklesCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
